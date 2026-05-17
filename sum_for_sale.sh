d662b0e45d1aeb18e0c9002f80e2d03c75ceb111
Kjetil Andresen <kjetil.andrese@gmail.com>
2026-05-17 11:19:50 +0200

    [collection] Add to collection

--- sum_for_sale.sh
+++ sum_for_sale.sh
@@ -2,6 +2,12 @@
 
 sum=$(yq '.[] | (.for_sale | tostring) +" "+ (.sold | tostring) + " "+ (.price | tostring)'  _data/records.yml \
   | grep "true false" \
-  | awk '{sum += $3} END {print sum * 0.91 * 11.71}')
+  | awk '{sum += $3} END {print sum * 10.82}')
 
-echo "$sum kr"
+echo "FOR SALE VALUE: $sum kr"
+
+sum=$(yq '.[] | (.for_sale | tostring) +" "+ (.sold | tostring) + " "+ (.price | tostring)'  _data/records.yml \
+  | grep "false false" \
+  | awk '{sum += $3} END {print sum * 10.82}')
+
+echo "COLLECTION VALUE: $sum kr"
